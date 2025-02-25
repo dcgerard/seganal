2ec0d67515263be273d81338e8ed300a05e7f5d4
dcgerard <gerard.1787@gmail.com>
2025-02-25 12:40:10 -0500

    alt output

--- Makefile
+++ Makefile
@@ -2,11 +2,16 @@ nc = 12
 rexec = R CMD BATCH --no-save --no-restore
 rout = ./output/rout
 
+## null sims output with outliers
 nullplots = ./output/nullsims/null_t1e.pdf \
             ./output/nullsims/qq_example.pdf
 
+## null sims output, no outliers
 nood_nullplots = ./output/nood_nullsims/nood_null_t1e.pdf \
                  ./output/nood_nullsims/nood_qq_example.pdf
+
+## alt sims output
+alt_output = ./output/altsims/alt_tab.txt
 
 .PHONY: all
 all: null_sims nood_null_sims alt_sims dr_null_sims
@@ -61,7 +66,12 @@ dr_null_sims: ./output/dr_nullsims/dr_null_pvalues.RDS
 
 ## Alternative simulations ----
 .PHONY: alt_sims
-alt_sims: ./output/altsims/alt_pvalues.RDS
+alt_sims: $(alt_output)
+
+$(alt_output): ./code/alt_plots.R ./output/altsims/alt_pvalues.RDS
+	mkdir -p $(rout)
+	mkdir -p $(@D)
+	$(rexec) $< $(rout)/$(basename $(<F)).Rout
 
 ./output/altsims/alt_pvalues.RDS: ./code/alt_sims.R
 	mkdir -p $(rout)
--- code/alt_plots.R
+++ code/alt_plots.R
@@ -1,6 +1,7 @@
 library(tidyverse)
-df <- readRDS("./output/nood_altsims/nood_alt_paramdf.RDS")
-pval <- readRDS("./output/nood_altsims/nood_alt_pvalues.RDS")
+library(xtable)
+df <- readRDS("./output/altsims/alt_paramdf.RDS")
+pval <- readRDS("./output/altsims/alt_pvalues.RDS")
 
 for (i in seq_along(pval)) {
   pval[[i]] <- as_tibble(pval[[i]])
@@ -12,6 +13,7 @@ bind_rows(pval) |>
   left_join(df, by = join_by(i)) |>
   mutate(n = as.factor(n)) |>
   filter(rd == 10) |>
+  select(-segtest_nout) |>
   pivot_longer(cols = c("segtest", "polymapR"), names_to = "method", values_to = "p") |>
   ggplot(aes(sample = p, color = method, lty = n)) +
   geom_qq(distribution = qunif, geom = "line") +
@@ -26,6 +28,7 @@ bind_rows(pval) |>
   left_join(df, by = join_by(i)) |>
   mutate(n = as.factor(n)) |>
   filter(is.infinite(rd)) |>
+  select(-segtest_nout) |>
   pivot_longer(cols = c("segtest", "polymapR"), names_to = "method", values_to = "p") |>
   ggplot(aes(sample = p, color = method, lty = n)) +
   geom_qq(distribution = qunif, geom = "line") +
@@ -36,3 +39,20 @@ bind_rows(pval) |>
   facet_grid(ploidy ~ scenario) +
   theme(strip.background = element_rect(fill = "white"))
 
+alpha <- 0.05
+bind_rows(pval) |>
+  left_join(df, by = join_by(i)) |>
+  mutate(
+    n = as.factor(n),
+    ploidy = as.factor(ploidy),
+    rd = as.factor(rd)) |>
+  select(-segtest_nout) |>
+  group_by(n, ploidy, rd, scenario) |>
+  summarize(
+    segtest = mean(segtest < alpha),
+    polymapR = mean(polymapR < alpha)) |>
+  xtable(
+    label = "tab:alt.power",
+    caption = "Results of the alternative simulations from Section \\ref{sec:alt.sims}. The table reports the power of \\texttt{segtest} and \\texttt{polymapR} under scenarios where segregation distortion is present (right two columns). Tests were performed at a nominal significance level of 0.05 across varying sample sizes ($n \\in \\{20, 200\\}$), ploidies ($K \\in \\{4, 6, 8\\}$), read depths (10 or infinite), and alternative scenarios (easy or hard). \\texttt{segtest} exhibits strong power for large $n$ and maintains reasonable power in most small $n$ scenarios. While \\texttt{segtest} has slightly lower power than \\texttt{polymapR} in most settings, this reduction is expected due to its ability to control Type I error, which \\texttt{polymapR} does not.") |>
+  print(include.rownames = FALSE) |>
+  cat(file = "./output/altsims/alt_tab.txt")
